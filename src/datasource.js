301ce5034ba35322f8441c45dec974c36ba80cac
chaolee50 <chaolee50@outlook.com>
2021-07-14 18:11:25 +0800

    support trace

--- src/datasource.js
+++ src/datasource.js
@@ -124,6 +124,7 @@ export function transformResponse(df) {
             { name: 'traceID', type: FieldType.string },
             { name: 'spanID', type: FieldType.string },
             { name: 'parentSpanID', type: FieldType.string },
+            { name: 'operationName', type: FieldType.string },
             { name: 'serviceName', type: FieldType.string },
             { name: 'serviceTags', type: FieldType.other },
             { name: 'startTime', type: FieldType.number },
