5caa86f7f13612ce451d1f0df5f7b3034ff95cd6
chaolee50 <chaolee50@outlook.com>
2022-11-28 15:03:36 +0800

    add field_search variable

--- src/datasource.ts
+++ src/datasource.ts
@@ -179,12 +179,12 @@ export function replaceQueryParameters(q: SLSQuery|string, options: DataQueryReq
     options.scopedVars,
     function (
       value: { forEach: (arg0: (v: string) => void) => void; join: (arg0: string) => void },
-      variable: { multi: any; includeAll: any; name: string; label: any }
+      variable: { multi: any; includeAll: any; name: string; label: any; description: string }
     ) {
       if (typeof value === 'object' && (variable.multi || variable.includeAll)) {
         const a: string[] = [];
         value.forEach(function (v: string) {
-          if (variable.name === variable.label) {
+          if (variable.name === variable.label || variable.description.indexOf('field_search') >= 0) {
             a.push('"' + variable.name + '":"' + v + '"');
           } else {
             a.push(v);
